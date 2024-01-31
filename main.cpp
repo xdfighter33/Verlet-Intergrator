4e2111a72a4695539e1a85ecaa72d245bc6d2fff
Isacs Macbook <Iwandiba@yahoo.com>
2024-01-31 09:44:22 -0500

    Fixing Set up timer

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@
 const int window_width = 1000;
 const int window_height = 1000; 
 
+bool spawn_delayz(sf::Time clock,float time){
+
+    if(clock.asSeconds() >= time ){
+
+            return true;
+    }
+
+};
 int main(){
 
 
@@ -21,16 +29,16 @@ int main(){
 
     simulator.setSubsStepscount(8);
     simulator.setSimulationUpdateRate(frame_rate);
-    const float x_spawn =  1000;
-    const float y_spawn =  1000;
+    const float x_spawn =  500;
+    const float y_spawn =  500;
     
     sf::Vector2f object_spawn_position = {x_spawn, y_spawn};
     const sf::Vector2f object_initial_speed = {0.0f,0.0f};
     const float object_min_radius = 10.0f;
     const float object_max_radius = 25.0f;
-    const float spawn_delay = .5f;
+    const float spawn_delay = 1.5f;
     const uint32_t max_object_count = 500;
-    const float max_angle = 1.0f;
+    const float max_angle = 10.0f;
 
 
 
@@ -41,7 +49,7 @@ int main(){
     sf::Clock clock;
     while(window.isOpen()){
 sf::Event events{};
-
+sf::Time clocks = clock.getElapsedTime();
 while(window.pollEvent(events)){
     if (events.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
     {
@@ -51,9 +59,10 @@ while(window.pollEvent(events)){
 
 
 }
-if (simulator.getObjectCount() < max_object_count  /* && clock.getElapsedTime().asSeconds()*/){
-    // clock.restart();
-    auto & object = simulator.addObject(object_spawn_position, object_min_radius);
+if (simulator.getObjectCount() < max_object_count && spawn_delayz(clock.getElapsedTime(),spawn_delay) == true ){
+     clock.restart();
+
+    auto & object = simulator.addObject( sin(max_angle) * object_spawn_position, object_min_radius);
 
 
 
