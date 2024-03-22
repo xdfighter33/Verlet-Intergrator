10098912a70f3e5e8a67e36b10ae4f87ed9251cb
Isac's Desktop <iwandiba@hotmail.com>
2024-03-22 14:38:14 -0400

    Fixing. creating grids

--- grid.hpp
+++ grid.hpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <stdint.h>
 
+class Grids{
+private:
+	int width;
+	int height; 
+	std::vector<std::vector<char>> grid;
+
+
+public: 
+
+	Grids(int w, int h) : width(w), height(h), grid(h, std::vector<char>(w, '.')) {}
+};
